b9de6a0a86393585d301e9319bf92a13dc4d3762
Ryan Crowley <70530712+rcrowl2@users.noreply.github.com>
2022-03-30 15:18:38 -0500

    Add files via upload

---
+++ sql/create_food_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Food (
+	food_id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255),
+    food_location INT NOT NULL,
+    quantity INT,
+    PRIMARY KEY (food_id),
+    FOREIGN KEY (food_location) REFERENCES location(location_id) ON UPDATE CASCADE ON DELETE CASCADE
+    );
