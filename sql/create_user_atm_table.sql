ce3653a0c4a45e86767120492634e52e449e4d0c
Ryan Crowley <70530712+rcrowl2@users.noreply.github.com>
2022-03-30 15:28:44 -0500

    Update create_user_atm_table.sql

--- sql/create_user_atm_table.sql
+++ sql/create_user_atm_table.sql
@@ -1,8 +1,7 @@
 CREATE TABLE user_atm (
 	atm_id INT NOT NULL,
     user_id INT NOT NULL,
-    PRIMARY KEY (atm_id),
-    PRIMARY KEY (user_id),
+    PRIMARY KEY (atm_id, user_id),
     FOREIGN KEY (atm_id) REFERENCES atm(atm_id),
     FOREIGN KEY (user_id) REFERENCES user(user_id)
     );
