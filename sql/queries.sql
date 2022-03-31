d89ec08380f674ad99f2d578387f78f013f26f25
Ansel Rosso Klakovich <rossoklakovich@gmail.com>
2022-03-30 21:32:48 -0500

    Update queries.sql

--- sql/queries.sql
+++ sql/queries.sql
@@ -11,9 +11,20 @@ where atm_id like '%45%'
 order by associated_warehouse;
 
 # aggregate queries
+create view aggregate_0 as
 SELECT SUM(balance) FROM user;
 
+create view aggregate_1 as
 SELECT user_id, COUNT(user_type) FROM user;
+
+create view aggregate_2 as
+select COUNT(warehouse_last_restocked)
+from warehouse;
+
+create view aggregate_3 as
+select COUNT(user()) as count, user_type as type
+from user
+group by user_type;
 
 
 # complex queries
