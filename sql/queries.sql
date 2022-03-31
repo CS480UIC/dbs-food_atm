0e95292fe86dae9621635f262bbe8e3e87e44332
Ryan Crowley <70530712+rcrowl2@users.noreply.github.com>
2022-03-30 21:32:15 -0500

    Update queries.sql

--- sql/queries.sql
+++ sql/queries.sql
@@ -11,7 +11,9 @@ where atm_id like '%45%'
 order by associated_warehouse;
 
 # aggregate queries
+SELECT SUM(balance) FROM user;
 
+SELECT user_id, COUNT(user_type) FROM user;
 
 
 # complex queries
